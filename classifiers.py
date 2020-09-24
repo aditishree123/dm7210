3111fff4a78da2344cca3326ff32e16b134f47d8
aditishree123 <44469551+aditishree123@users.noreply.github.com>
2020-09-25 02:24:03 +0530

    bayesian ridge

--- classifiers.py
+++ classifiers.py
@@ -5,16 +5,16 @@ from sklearn.linear_model import LinearRegression
 btc=pd.read_csv('cleaned_dataset.csv')
 features = ['Open', 'High', 'Low', 'Volume_(BTC)', 'Volume_(Currency)', 'Weighted_Price']
 label = 'Close'
-#dividing dataset in 70%test,15%train and 15% vlidation
+#dividing dataset in 60%test,20%train and 20% vlidation
 X_train, X_test, Y_train, Y_test = train_test_split(
     btc[features],
     btc[label],
-    test_size = 0.15
+    test_size = 0.40
 )
-X_train, X_val, Y_train, Y_val = train_test_split(
+X_test, X_val, Y_test, Y_val = train_test_split(
     btc[features],
     btc[label],
-    test_size = 0.15
+    test_size = 0.50
 )
 
 print(X_train.size)
@@ -25,3 +25,9 @@ print(X_val.size)
 lr = LinearRegression()
 lr.fit(X_train, Y_train)
 print(lr.score(X_test, Y_test))
+
+#Bayesian Ridge regression
+from sklearn.linear_model import BayesianRidge
+gnb = BayesianRidge()
+gnb.fit(X_train, Y_train)
+print(gnb.score(X_test,Y_test))
