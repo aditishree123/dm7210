2584fe2f5d81743c2194f42f7355546630987810
Meera Joshi <44469405+meera-joshi@users.noreply.github.com>
2020-09-24 23:39:47 +0530

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,9 +7,16 @@ if __name__ == '__main__':
     #Data Cleaning
     DF = pd.DataFrame(data)
     
+    #Percentage of rows having NULL values
+    print("Percentage of NULL values in each column:")
+    print(DF.isnull().mean()*100)
+    #Count of rows having no NULL values
+    print("Count of non NULL values in each column:")
+    print(DF.count())
+    
     #Dropping NaN value Tuples
     DF.dropna(axis=0, how='any', thresh=None, subset=None, inplace=True)
     
-    #Converting UNIX timestamp to date and time format"
+    #Converting UNIX timestamp to date and time format
     DF['dateTime'] = pd.to_datetime(DF['Timestamp'], unit='s')
     DF.to_csv('cleaned_dataset.csv')
